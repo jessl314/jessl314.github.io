debf5e8b21f3fa15dac6d14090962c33be28f289
jessl314 <jlchrysanthemum456@gmail.com>
2025-08-04 15:35:52 -0700

    basic mongodb established

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
+const cors = require('cors')
 require('dotenv').config();
 
 const app = express()
@@ -7,26 +8,25 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
+// Middleware to handle frontend/backend connection
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+// connection uri to mongodb cluster/cloud
 const uri = process.env.MONGO_URI;
 
-// Creates a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+}).catch(err => console.error(err));
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('database connection established successfully');
+}).on('error', (error) => {
+    console.log("database connection error: ", error);
 });
 
-async function run() {
-  try {
-    // Connect the client to the server
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
