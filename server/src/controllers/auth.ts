66c2ed4a3e4a4d7d2a0311c78b516109fd028532
jessl314 <jlchrysanthemum456@gmail.com>
2026-07-04 13:52:56 -0700

    restarting my portfolio

--- server/src/controllers/auth.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-
-export const authController = {
-    // Add your authentication methods here
-    // Example:
-    // login: async (req: Request, res: Response) => { ... },
-    // register: async (req: Request, res: Response) => { ... }
-};
